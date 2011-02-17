af029255ee4fa813d2d9942579f44b3650e458de
unknown <Bas@.(none)>
2011-02-17 20:44:26 +0100

    update to 0.2

--- SpringMobileGrailsPlugin.groovy
+++ SpringMobileGrailsPlugin.groovy
@@ -10,12 +10,10 @@ import org.springframework.mobile.device.wurfl.WurflDeviceResolver;
 
 class SpringMobileGrailsPlugin {
 	// the plugin version
-	def version = "0.1"
+	def version = "0.2"
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "1.3.6 > *"
-	// the other plugins this plugin depends on
-	def dependsOn = [filters:"1.3.6"]
-	def observe = ["filters"]
+	
 	// resources that are excluded from plugin packaging
 	def pluginExcludes = [
 		"grails-app/views/error.gsp"
