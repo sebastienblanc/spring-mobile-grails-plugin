7015579ea9fcc7a37a736ed2f9fbd5c81c18957c
Burt Beckwith <burt@burtbeckwith.com>
2013-10-25 00:43:40 -0400

    no need to copy wurfl files since they were removed

--- scripts/_Install.groovy
+++
@@ -1,7 +0,0 @@
-ant.mkdir(dir:"$basedir/grails-app/web-app/WEB-INF/wurfl")
-
-ant.copy(file:"$springMobilePluginDir/grails-app/conf/wurfl/web_browsers_patch.xml",
-         todir:"$basedir/web-app/WEB-INF/wurfl")
-
-ant.copy(file:"$springMobilePluginDir/grails-app/conf/wurfl/wurfl-2.0.25.zip",
-         todir:"$basedir/web-app/WEB-INF/wurfl")
